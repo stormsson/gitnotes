54c7c54d9d9739d9398707669d3396952a7ca5a6
Camillo Camarda <thorsten82@gmail.com>
2017-03-09 19:01:07 +0100

    =inizio attivita'

--- index.php
+++ index.php
@@ -3,6 +3,7 @@ require_once __DIR__.'/vendor/autoload.php';
 
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
 
 $app = new Silex\Application();
 
@@ -10,8 +11,42 @@ $app->get('/hello/{name}', function($name) use($app) {
     return 'Hello '.$app->escape($name);
 });
 
-$app->post('/push',function(Request $request) use $app{
-    $message = $request->get('message');
+$app->post('/push',function(Request $request) use ($app){
+    $client = new \Github\Client();
+
+
+    if (0 === strpos($request->headers->get('Content-Type'), 'application/json')) {
+        $data = json_decode($request->getContent(), true);
+
+    } else {
+        throw new AccessDeniedHttpException("403"   );
+    }
+
+    $repoName = $data['repository']['name'];
+    $repoOwner = $data['repository']['owner']['name'];
+
+
+    foreach ($data['commits'] as $commit) {
+        $commitSHA = $commit['id'];
+        $author = $commit['author'];
+
+        $commitObj = $client->api('repo')
+            ->commits()
+            ->show($repoOwner, $repoName, $commitSHA);
+
+        foreach ($commitObj['files'] as $file) {
+            $filePatch = $file['patch'];
+            $fileName = $file['filename'];
+        }
+        die(var_dump($commitObj));
+
+
+
+    }
+
+
+
+    $payload = $request->request->all();
 
     return new Response('Thank you for your feedback!', 201);
 });
